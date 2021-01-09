d45cefec7037f6f3d151df718d9509f4d42ed645
Ramon Fischer <Ramon_Fischer@hotmail.de>
2021-01-09 19:06:42 +0100

    create the respective directory

--- update_list.sh
+++ update_list.sh
@@ -2,7 +2,7 @@
 
 source "/usr/local/lib/functions.sh"
 
-COMMAND_ARRAY=("/usr/bin/find" "/bin/ls" "/bin/mount" "/usr/bin/sort" "/bin/umount")
+COMMAND_ARRAY=("/usr/bin/find" "/bin/ls" "/bin/mkdir" "/bin/mount" "/usr/bin/sort" "/bin/umount")
 checkCommands
 
 # define global variables
@@ -19,6 +19,11 @@ if ! beQuiet "" "mountpoint ${mount_point}"
 then
     /bin/mount "${mount_point}"
 else
+    if [[ ! -f "${directory_path}" ]]
+    then
+        /bin/mkdir "${directory_path}"
+    fi
+
     echo -e "# ${current_year}\n" > "${readme_file}"
     echo '```no-highlight' >> "${readme_file}"
     /usr/bin/find "${document_directory}" -type f -name "*.png" -newermt "${current_year}${first_month}${first_day}" -and -not -newermt "$(( ${current_year} + 1 ))${first_month}${first_day}" -exec /bin/ls -l "{}" + | /usr/bin/sort --key="6,6M" --key="7,7n" >> "${readme_file}"
